6d36f1a32a07392b585044d549ba2d2a13f0fc71
lord-cyclone100 <sang66deb@gmail.com>
2025-06-17 20:34:36 +0530

    Added output image for Singleton class

--- week1/Design Patterns and Principles/SingletonPatternExample/Test.java
+++ week1/Design Patterns and Principles/SingletonPatternExample/Test.java
@@ -1,16 +1,16 @@
-class Logger{
-    private Logger(){}
-    private static Logger lg = new Logger();
-    public static Logger getInstance(){
-        return lg;
+class Logger{       //Singleton class
+    private Logger(){}      //Constructor made private to prevent instantiation using new keyword by other classes
+    private static Logger lg = new Logger();        //The single object returned every time
+    public static Logger getInstance(){     //Function to return the object
+        return lg;                          //made static so that it can be called without object creation
     }
 }
 
 public class Test{
     public static void main(String[] args){
-        Logger l1 = Logger.getInstance();
-        Logger l2 = Logger.getInstance();
+        Logger l1 = Logger.getInstance();       //Creating l1 instance
+        Logger l2 = Logger.getInstance();       //Creating l2 instance
         System.out.println(l1);
-        System.out.println(l2);
+        System.out.println(l2);     //Both the print statements print the same hexadecimal representation of the hash code
     }
 }
