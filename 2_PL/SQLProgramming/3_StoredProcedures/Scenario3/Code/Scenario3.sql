dafe9c878bc4dcd03b3cb6d6ea798d37c02503a0
lord-cyclone100 <sang66deb@gmail.com>
2025-06-26 10:36:26 +0530

    completed pl/sql stored procedures scenario 3

--- 2_PL/SQLProgramming/3_StoredProcedures/Scenario3/Code/Scenario3.sql
+++ 2_PL/SQLProgramming/3_StoredProcedures/Scenario3/Code/Scenario3.sql
@@ -1,8 +1,25 @@
-declare
-  cursor cs is
-    select * from customer_loan_due where due_date between sysdate and sysdate+30;
+create or replace procedure TransferFunds (
+    src_account in number,
+    dest_account   in number,
+    amount       in number
+) is
+    rem_balance number(10,2);
 begin
-  for cust in cs loop
-    dbms_output.put_line(cust.name || ', You have less than 30 days to repay your loan. Due date :- ' || cust.due_date);
-  end loop;
+    -- Get balance of source account
+    select balance into rem_balance from bank_accounts where account_id = src_account for update;
+
+    -- Check for sufficient balance
+    if rem_balance < amount then
+        raise_application_error(-20001, 'Insufficient funds in the source account.');
+    end if;
+
+    -- Deduct from source
+    update bank_accounts set balance = balance - amount where account_id = src_account;
+
+    -- Add to target
+    update bank_accounts set balance = balance + amount where account_id = dest_account;
+
+    commit;
+
+    dbms_output.put_line('Transferred $' || amount || ' from account ' || src_account || ' to account ' || dest_account);
 end;
