2e5b32815b281e3d79e60a4aaad95346eaf5e0db
lord-cyclone100 <sang66deb@gmail.com>
2025-06-27 14:23:41 +0530

    Completed code for Mocking and Stubbing

---
+++ 2_TDDUsingJunit5andMockito/1_MockingAndStubbing/Code/src/main/java/org/example/services/MyService.java
@@ -0,0 +1,12 @@
+package org.example.services;
+
+public class MyService {
+    private ExternalApi externalApi;
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}
