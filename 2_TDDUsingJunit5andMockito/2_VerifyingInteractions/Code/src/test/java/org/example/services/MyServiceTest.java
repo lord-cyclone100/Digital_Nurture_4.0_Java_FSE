ef776c66f108efddaa164a7fa81d765bcd7fb223
lord-cyclone100 <sang66deb@gmail.com>
2025-06-27 14:49:32 +0530

    Completed code for Verifying Interactions

---
+++ 2_TDDUsingJunit5andMockito/2_VerifyingInteractions/Code/src/test/java/org/example/services/MyServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.services;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+    @Test
+    public void testVerifyInteraction(){
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
