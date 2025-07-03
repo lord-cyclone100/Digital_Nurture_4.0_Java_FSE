20e807bb8de969fc8c62e9f87983bc7dfd9260f2
lord-cyclone100 <sang66deb@gmail.com>
2025-07-03 10:33:25 +0530

    Added code for Configuring a basic Spring Application

---
+++ 3_SpringCoreAndMaven/1_ConfiguringABasicSpringApplication/Code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepo;
+    public void setBookRepository(BookRepository bookRepo){
+        this.bookRepo = bookRepo;
+    }
+    public void printBookTitle(){
+        System.out.println("Book Title : "+ bookRepo.getBookTitle());
+    }
+}
