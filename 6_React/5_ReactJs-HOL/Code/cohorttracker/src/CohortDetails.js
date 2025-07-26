72150b4d82cee53cc5f7987003f405f03331f0b8
lord-cyclone100 <sang66deb@gmail.com>
2025-07-26 08:46:50 +0530

    Added code for react js hol 5

---
+++ 6_React/5_ReactJs-HOL/Code/cohorttracker/src/CohortDetails.js
@@ -0,0 +1,27 @@
+import styles from './cohortdetails.module.css'
+function CohortDetails(props) {
+    const h3Style = {
+        color:props.cohort.currentStatus === 'Ongoing' ? 'green' :'blue'
+    }
+    return (
+        <div className={styles.box}>
+            <h3 style={h3Style}>
+                {props.cohort.cohortCode} -
+                <span>{props.cohort.technology}</span>
+            </h3>
+            <dl>
+                <dt>Started On</dt>
+                <dd>{props.cohort.startDate}</dd>
+                <dt>Current Status</dt>
+                <dd>{props.cohort.currentStatus}</dd>
+                <dt>Coach</dt>
+                <dd>{props.cohort.coachName}</dd>
+                <dt>Trainer</dt>
+                <dd>{props.cohort.trainerName}</dd>
+            </dl>
+        </div>
+
+        
+    );
+}
+export default CohortDetails;
